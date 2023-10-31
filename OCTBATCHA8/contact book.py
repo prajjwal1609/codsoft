a06230332f089c3c712b8024090d9d4c6cd2ada8
prajjwal1609 <149517475+prajjwal1609@users.noreply.github.com>
2023-10-31 22:32:31 +0530

    contact book.py

--- OCTBATCHA8/contact book.py
+++ OCTBATCHA8/contact book.py
@@ -1,6 +1,3 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>># Create an empty dictionary to store contacts
 contacts = {}
 
 # Function to add a contact
