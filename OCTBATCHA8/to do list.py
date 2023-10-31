7a7d48f8ff2c522fe9dc7be68ae5c499cd380b75
prajjwal1609 <149517475+prajjwal1609@users.noreply.github.com>
2023-10-31 22:31:46 +0530

    to do list.py

--- OCTBATCHA8/to do list.py
+++ OCTBATCHA8/to do list.py
@@ -1,2 +1,42 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+tasks = []
+def add_task():
+    task = input("Enter task: ")
+    tasks.append(task)
+    print("Task added!")
+
+def show_tasks():
+    if not tasks:
+        print("No tasks in the list.")
+    else:
+        print("Tasks:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+
+def remove_task():
+    show_tasks()
+    task_number = int(input("Enter the task number to remove: "))
+    if 1 <= task_number <= len(tasks):
+        removed_task = tasks.pop(task_number - 1)
+        print(f"Removed: {removed_task}")
+    else:
+        print("Invalid task number.")
+
+while True:
+    print("\nOptions:")
+    print("1. Add a task")
+    print("2. Show tasks")
+    print("3. Remove a task")
+    print("4. Quit")
+    
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        add_task()
+    elif choice == '2':
+        show_tasks()
+    elif choice == '3':
+        remove_task()
+    elif choice == '4':
+        break
+    else:
+        print("Invalid choice. Please try again.")
